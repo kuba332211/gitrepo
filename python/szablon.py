8f76b070623a1807d45f0a0ffb25609b5094251c
kl2cg1 <kuba1122339@gmail.com>
2018-09-11 12:21:12 +0200

    dodanie szablonu pythona

---
+++ python/szablon.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  szablon.py
+#  
+
+
+def main(args):
+    print("Hello")
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
