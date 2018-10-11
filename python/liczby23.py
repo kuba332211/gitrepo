5c22cc312021a3711c2dd65315f1dc4b0dbfa7bb
kl2cg1 <kuba1122339@gmail.com>
2018-10-11 07:57:45 +0200

    potegait

--- python/liczby23.py
+++ python/liczby23.py
@@ -11,10 +11,22 @@ def liczby2():
                 print("{}{} ".format(i, j), end="")
                 ile = ile + 1 
     return ile
+    
+    
+def liczby3():
+    ile = 0 
+    for i in range(1, 10):
+        for j in range(10):
+            for k  in range(10):
+                if i != j and i != k and j != k: 
+                    print("{}{}{} ".format(i, j, k), end="")
+                ile = ile + 1 
+    return ile
         
         
 def main(args):
-    print("/n/nLiczb 2-cyfrowych:", liczby2())
+    liczby3
+    print("/n/nLiczb 2-cyfrowych:", liczby3())
     return 0
 
 if __name__ == '__main__':
